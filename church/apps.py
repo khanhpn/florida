f9080c00df09f47f441cc782173e72de58e4c7d6
khanhpn <phamkhanh90@gmail.com>
2020-12-30 07:36:05 +0700

    initial project django church

---
+++ church/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChurchConfig(AppConfig):
+    name = 'church'
---
+++ florida/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for florida project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'florida.settings')
+
+application = get_wsgi_application()
